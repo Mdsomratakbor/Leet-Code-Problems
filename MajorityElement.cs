2e396b265f03b41625cece20c81107627acf5761
Mdsomratakbor <somrat876@gmail.com>
2022-05-21 23:45:07 +0600

    Majority Element problem solve base on O(n) running times

--- MajorityElement.cs
+++ MajorityElement.cs
@@ -8,6 +8,8 @@ namespace LeetCodeProblems
 {
     public class MajorityElement
     {
+
+        //Complexity O(n^2)
         public int MajorityElementSolution(int[] nums)
         {
             int count = 0, maxCount = 0, index = -1;
@@ -31,5 +33,50 @@ namespace LeetCodeProblems
             return nums[index];
 
         }
+
+
+
+
     }
+
+
+    // complexity O(n)
+    //public class Solution
+    //{
+    //    public int MajorityElement(int[] nums)
+    //    {
+    //        if (nums == null || nums.Length == 0)
+    //            return -1;
+
+    //        var length = nums.Length;
+    //        var majority = nums[0];
+    //        var count = 1;
+    //        var hasMajority = true;
+    //        for (int i = 1; i < length; i++)
+    //        {
+    //            var current = nums[i];
+    //            if (hasMajority)
+    //            {
+    //                if (current == majority)
+    //                {
+    //                    count++;
+    //                }
+    //                else
+    //                {
+    //                    count--;
+    //                }
+
+    //                hasMajority = count > 0;
+    //            }
+    //            else
+    //            {
+    //                count = 1;
+    //                hasMajority = true;
+    //                majority = nums[i];
+    //            }
+    //        }
+
+    //        return majority;
+    //    }
+    //}
 }
