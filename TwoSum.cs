d55aa2853849f1674f8c332251b5ca15f2e03ac0
Mdsomratakbor <somrat876@gmail.com>
2022-05-21 16:34:06 +0600

    Project initialize and TwoSum  problem solve

---
+++ TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class TwoSum
+    {
+        public int[] TwoSumSolution(int[] nums, int target)
+        {
+            int[] arrayOfIndex = new int[2];
+            for(int i=0; i<nums.Length; i++)
+            {
+                for(int j=i+1; j<nums.Length; j++)
+                {
+                    if(nums[i]+nums[j]== target)
+                    {
+                        arrayOfIndex[0] = i;
+                        arrayOfIndex[1] = i;
+                    }
+                }
+            }
+            return arrayOfIndex;
+        }
+    }
+}
