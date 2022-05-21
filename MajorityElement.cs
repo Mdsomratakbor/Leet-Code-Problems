47a8272d0f15cd3da0ebc738e4c2aac86b733ac2
Mdsomratakbor <somrat876@gmail.com>
2022-05-21 23:39:42 +0600

    Majority Element problem solve

---
+++ MajorityElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class MajorityElement
+    {
+        public int MajorityElementSolution(int[] nums)
+        {
+            int count = 0, maxCount = 0, index = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                count = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    index = i;
+                }
+
+            }
+            return nums[index];
+
+        }
+    }
+}
