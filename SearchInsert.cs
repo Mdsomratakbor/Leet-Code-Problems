8ffd3e8363c0ed627e61e66bcdb0ead0ce85fe5e
Mdsomratakbor <somrat876@gmail.com>
2022-05-21 23:14:57 +0600

    Search Insert problem solve

---
+++ SearchInsert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+   public class SearchInsert
+    {
+        public int SearchInsertSolution(int[] nums, int target)
+        {
+            int output = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                if (nums[i] == target)
+                {
+                    output = i;
+                    break;
+                }
+                else if (nums[i] > target)
+                {
+                    output = i;
+                    break;
+                }
+                else
+                {
+                    output = i + 1;
+
+                }
+
+            }
+            return output;
+        }
+    }
+}
