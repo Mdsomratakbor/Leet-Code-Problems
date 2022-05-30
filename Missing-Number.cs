d2a4353936a76e347ada9fac71d59ca950527955
284976109193MSA <vdr_makbar@parata.com>
2022-05-30 19:24:59 +0600

    some new problem solve

---
+++ Missing-Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Missing_Number
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int lengthOfArray = nums.Length;
+            Array.Sort(nums);
+            int output = 0;
+            for (int i = 0; i < lengthOfArray; i++)
+            {
+                if (nums[i] != i)
+                {
+                    return i;
+
+                }
+
+            }
+            return lengthOfArray;
+
+        }
+    }
+}
---
+++ Move-Zeroes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Move_Zeroes
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int current = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[current] = nums[i];
+                    current++;
+                }
+            }
+            while (current < nums.Length)
+            {
+                nums[current] = 0;
+                current++;
+            }
+        }
+    }
+}
