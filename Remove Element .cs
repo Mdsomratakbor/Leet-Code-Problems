8c4daea135242b5d49c8293a72ffabf8c508aa5f
Md Somrat Akbor <53125546+Mdsomratakbor@users.noreply.github.com>
2022-07-08 00:49:12 +0600

    Create Remove Element .cs

---
+++ Remove Element .cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+     
+        int arraySize =nums.Length; 
+        if(arraySize==0)
+            return 0;
+        int valid_size=0;
+        for(int i=0; i<arraySize; i++){
+            if(nums[i]!= val)
+               nums[ valid_size++]=nums[i];
+        }
+        return valid_size;
+    }
+}
