834f00333fbe93232f009c4bf0da2af39e267f3b
284976109193MSA <vdr_makbar@parata.com>
2022-07-31 14:15:19 +0600

    FirstBadVersion.cs file add

---
+++ FirstBadVersion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class FirstBadVersion 
+    {
+        public int FirstBadVersionSolution(int n)
+        {
+            int left = 1;
+            int right = n;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (IsBadVersion(mid))
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            return left;
+        }
+        public bool IsBadVersion(int value)
+        {
+            return value == 0;
+        }
+    }
+}
