18821e24e7eb678259266d7d8f8eefa7c5a5047e
Mdsomratakbor <somrat876@gmail.com>
2022-08-01 00:14:04 +0600

    LongestCommonPrefix.cs problem solve

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class LongestCommonPrefix
+{
+        public string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder result = new StringBuilder();
+            int firstStringLength = strs[0].Length;
+            int arrayLength = strs.Length;
+            for (int i = 0; i < firstStringLength; i++)
+            {
+
+                for (int j = 1; j < arrayLength; j++)
+                {
+                    if (strs[j].Length <= i)
+                        return result.ToString();
+
+                    if (strs[0][i] != strs[j][i])
+                    {
+                        return result.ToString();
+                    }
+                }
+                result.Append(strs[0][i]);
+            }
+            return result.ToString();
+        }
+    
+}
