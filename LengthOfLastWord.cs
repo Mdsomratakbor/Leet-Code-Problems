8b41d20d3c215b991c02850d0e8b6351cf02e179
284976109193MSA <vdr_makbar@parata.com>
2022-07-31 14:11:48 +0600

    Length of Last word problem solve

---
+++ LengthOfLastWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class LengthOfLastWord
+    {
+        public int LengthOfLastWordSolution(string s)
+        {
+
+            string[] arrayOfString = s.Trim().Split();
+            return arrayOfString[arrayOfString.Length - 1].Length;
+        }
+    }
+}
