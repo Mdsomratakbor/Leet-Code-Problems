f0d23446179aedca89aacb9c5678aa9372865cd0
Md Somrat Akbor <53125546+Mdsomratakbor@users.noreply.github.com>
2022-08-24 22:11:29 +0600

    Create ThirdMaximumNumber.cs

---
+++ ThirdMaximumNumber.cs
@@ -0,0 +1,11 @@
+   public class ThirdMaximumNumber{
+   public int ThirdMax(int[] nums) {
+          var arr =nums.Distinct().ToArray();  
+      arr = arr.OrderByDescending(c => c).ToArray();
+        if(arr.Length<3)
+        return arr[0];
+        else
+            return arr[2];
+        
+    }
+    }
