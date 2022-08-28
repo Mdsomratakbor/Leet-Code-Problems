586bfed0725e62ed92115a422a76656ab64ef7b4
Md Somrat Akbor <53125546+Mdsomratakbor@users.noreply.github.com>
2022-08-28 19:38:01 +0600

    Create AddDigit.cs

---
+++ AddDigit.cs
@@ -0,0 +1,15 @@
+public class AddDigit {
+    public int AddDigits(int num) {
+        int sum=0;
+        while(num>0 || sum>9){
+             if (num == 0)
+            {
+                num = sum;
+                sum = 0;
+            }
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
