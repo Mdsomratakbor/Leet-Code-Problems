ef41870e4a5f53033e0276b553bc22de69ba4e1c
MdSomratAkbor <somrat876@gmail.com>
2022-10-20 17:18:06 +0600

    Divide Two Integer problem solve

---
+++ Divide-Two-Integers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Divide_Two_Integers
+    {
+        public int Divide(int dividend, int divisor)
+        {
+
+           
+            if (divisor == 0)
+                return Int32.MaxValue;
+            if (divisor == -1 && dividend == Int32.MinValue)
+                return Int32.MaxValue;
+
+            int sign = (dividend<0 ^ divisor<0)?-1: 1;
+            int count = 0;
+            long dividend2 = Math.Abs((long)dividend);
+            long divisor2 = Math.Abs((long)divisor);
+            while (dividend2 >= divisor2)
+            {
+                dividend2 -= divisor2;
+                count++;
+            
+            }
+            count = sign == -1  ? -count : count;
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            Sqrt data = new();
+            Divide_Two_Integers data = new();
             while (true)
             {
-              var input =  Console.ReadLine();
-                Console.WriteLine(data.MySqrt(Convert.ToInt32(input)));
+                var input = Console.ReadLine().Split(' ');
+                Console.WriteLine(data.Divide(Convert.ToInt32(input[0]), Convert.ToInt32(input[1])));
             }
           
         }
