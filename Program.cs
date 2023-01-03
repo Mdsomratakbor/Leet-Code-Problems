c30c2eec0ceace20a00f6d5b1eb00d8e5307b759
MdSomratAkbor <somrat876@gmail.com>
2023-01-03 14:06:33 +0600

    d

--- Program.cs
+++ Program.cs
@@ -6,14 +6,35 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            JumpGame data = new();
-           // while (true)
-         //   {
-               // var input = Console.ReadLine().Split(' ');
-                int[] array =  new int[] { 3,2,1,0,4 };
-                Console.WriteLine(data.CanJump(array));
-           // }
-          
+            Permutation data = new();
+            // while (true)
+            //   {
+            // var input = Console.ReadLine().Split(' ');
+            // Given array
+            char[,] grid = { { 'A','B','C','E' },
+                   { 'S','F','C','S' },
+                   {  'A','D','E','E'},
+            {  'A','D','E','E'}};
+            int ROW = 4;
+            int COL = 4;
+            // Declare the visited array
+            bool[,] vis = new bool[ROW, COL];
+         
+            // path from arr[0][0]
+            // to arr[row][col]
+          var lists=  data.Permute(new int[] { 1, 2, 3 });
+            Console.WriteLine("[");
+            foreach (var list in lists)
+            {
+                Console.WriteLine($"    [{string.Join(',', list)}]");
+            }
+            Console.WriteLine("]");
+            //if (data.BFS(grid, vis, 0, 0))
+            //   Console.WriteLine("Yes");
+            // else
+            //  Console.WriteLine("No");
+            // }
+
         }
     }
 }
