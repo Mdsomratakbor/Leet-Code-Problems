e82734fb1d5ae1b070ee5b525f8a2410236acfe2
MdSomratAkbor <somrat876@gmail.com>
2023-01-04 16:06:46 +0600

    Reverse Words in a String problem solve

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            ValidIPAddressChecker data = new();
+            ReverseWordsinString data = new();
 
-            Console.WriteLine(data.ValidIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
+            Console.WriteLine(data.ReverseWords("dog cat cat fish"));
           
 
         }
---
+++ WordPattern.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class WordPatternCheck
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] patternData = s.Split(' ');
+
+            int patternDataLength = patternData.Length;
+            if (patternDataLength != pattern.Length)
+                return false;
+
+            List<KeyValuePair<char, string>> list = new List<KeyValuePair<char, string>>();
+            for (int i = 0; i < patternDataLength; i++)
+            {
+
+                list.Add(new KeyValuePair<char, string>(pattern[i], patternData[i]));
+
+            }
+            int index = 0;
+            foreach (KeyValuePair<char, string> pair in list)
+            {
+                if (pair.Key == pattern[index] && pair.Value == patternData[index])
+                {
+                    index++;
+                    continue;
+                
+                }
+                else
+                {
+                   return false;
+                }
+            
+            }
+            return true;
+        }
+    }
+}
