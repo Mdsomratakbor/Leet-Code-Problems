430b4bd94541ecfe36f37cb164fbae7f1666ad1b
MdSomratAkbor <somrat876@gmail.com>
2023-01-04 12:57:59 +0600

    validIPAddress Problem solve

--- Program.cs
+++ Program.cs
@@ -6,35 +6,10 @@ namespace LeetCodeProblems
     {
         static void Main(string[] args)
         {
-            Strong_Password_Checker_II data = new();
-            // while (true)
-            //   {
-            // var input = Console.ReadLine().Split(' ');
-            // Given array
-            char[,] grid = { { 'A','B','C','E' },
-                   { 'S','F','C','S' },
-                   {  'A','D','E','E'},
-            {  'A','D','E','E'}};
-            int ROW = 4;
-            int COL = 4;
-            // Declare the visited array
-            bool[,] vis = new bool[ROW, COL];
-         
-            // path from arr[0][0]
-            // to arr[row][col]
-            Console.WriteLine(data.StrongPasswordCheckerII("yvTY#@IB#*!hjrQt-TLW&z)$@!%Duqt&ToskxHgnybqpndMI+wP&fcemIk#@KnkMTaUkcIbncpTL"));
-          //var lists=  data.Permute(new int[] { 1, 2, 3 });
-            //Console.WriteLine("[");
-            //foreach (var list in lists)
-            //{
-            //    Console.WriteLine($"    [{string.Join(',', list)}]");
-            //}
-            //Console.WriteLine("]");
-            //if (data.BFS(grid, vis, 0, 0))
-            //   Console.WriteLine("Yes");
-            // else
-            //  Console.WriteLine("No");
-            // }
+            ValidIPAddressChecker data = new();
+
+            Console.WriteLine(data.ValidIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
+          
 
         }
     }
