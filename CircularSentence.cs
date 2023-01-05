f74e9d88c7089c3164b4f73f352fe5206fad1bb8
MdSomratAkbor <somrat876@gmail.com>
2023-01-05 13:12:08 +0600

    circular sentence problem solve

---
+++ CircularSentence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class CircularSentence
+    {
+        public bool IsCircularSentence(string sentence)
+        {
+            var sentenceArray = sentence.Split(' ');
+            int i = 0;
+            int length = sentenceArray.Length;
+            if (length == 1)
+            {
+                if (sentenceArray[i][i] == sentenceArray[i].LastOrDefault())
+                {
+                    return true;
+                }
+            }
+            while (i < length-1)
+            {
+          
+                if ((sentenceArray[i].LastOrDefault() != sentenceArray[i+1].FirstOrDefault()))
+                {
+                    return false;
+                }
+                i++;
+            }
+      
+            return sentenceArray[0].FirstOrDefault() == sentenceArray[length - 1].LastOrDefault();
+        }
+    }
+}
