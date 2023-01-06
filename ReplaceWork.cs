eb6bfd1f2a27cc36e56e7cfc4be71ff79161125f
MdSomratAkbor <somrat876@gmail.com>
2023-01-07 00:03:43 +0600

    replace word problem solving start

---
+++ ReplaceWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class ReplaceWork
+    {
+        public string ReplaceWords(IList<string> dictionary, string sentence)
+        {
+            var sentenceArray = sentence.Split( ' ');
+
+            foreach (var word in dictionary)
+            {
+
+            }
+            return "";
+        }
+    }
+}
