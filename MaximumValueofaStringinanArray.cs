87e36d0ceb2019b3d279b3139b18d057d69b6c4d
MdSomratAkbor <somrat876@gmail.com>
2023-01-08 13:51:28 +0600

    maximum value of a string in a Array problem is solved

---
+++ MaximumValueofaStringinanArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class MaximumValueofaStringinanArray
+    {
+        public int MaximumValue(string[] strs)
+        {
+            int maxNumber = 0;
+            for(int i=0; i < strs.Length; i++)
+            {
+                int number = 0;
+                if (Int32.TryParse(strs[i],out number))
+                {
+                    if(number>maxNumber)
+                        maxNumber = number;
+                }
+                else
+                {
+                    if (strs[i].Length > maxNumber)
+                    {
+                        maxNumber = strs[i].Length;
+                    }
+                }
+            }
+            return maxNumber;
+        }
+    }
+}
