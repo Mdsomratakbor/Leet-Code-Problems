c2d8dd8beba53fa144fad56d67beec1fac3e9c5d
MdSomratAkbor <somrat876@gmail.com>
2023-01-08 17:22:56 +0600

    Percentage of Letter in String problem soved

---
+++ CountPairsOfSimilarStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class CountPairsOfSimilarStrings
+    {
+        public int SimilarPairs(string[] words)
+        {
+            int count = 0;
+            for(int i=0;i<words.Length; i++)
+            {
+                for(int j=i;j<words.Length; j++)
+                {
+                    if (words[i].OrderBy(x=>x).ToString() == words[j].OrderBy(x=>x).ToString())
+                    {
+                        count++;
+                        break;
+                    }
+                        
+
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ PercentageofLetterinString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class PercentageofLetterinString
+    {
+        public int PercentageLetter(string s, char letter)
+        {
+            double length = s.Length;
+            double count = 0;
+            for(int i=0;i<length; i++)
+            {
+                if (s[i]==letter)
+                    count++;
+            }
+            return (int) ((count / length) * 100);
+        }
+    }
+}
