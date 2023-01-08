715e700b041446911b83e90507eade500ccb4ce7
MdSomratAkbor <somrat876@gmail.com>
2023-01-08 13:12:49 +0600

    replace word update

--- ReplaceWord.cs
+++ ReplaceWord.cs
@@ -24,7 +24,7 @@ namespace LeetCodeProblems
                         if (sentenceArray[i].StartsWith(word))
                         {
                             sentenceArray[i] = word;
-                            isReplace = true;
+                            //isReplace = true;
                             break;
 
                         }
