edb764a4008a94073f2b9b4ee59bf8190d7cd19b
MdSomratAkbor <somrat876@gmail.com>
2023-03-28 14:29:19 +0600

    Binary Tree Inorder traversal problem solve

---
+++ BinaryTreeInorderTraversal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode currentNode= root;
+            while (currentNode != null || stack.Count != 0)
+            {
+                while(currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.left;
+                }
+                if (stack.Count != 0) {
+                currentNode = stack.Pop();
+                    result.Add(currentNode.val);
+                    currentNode= currentNode.right;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
