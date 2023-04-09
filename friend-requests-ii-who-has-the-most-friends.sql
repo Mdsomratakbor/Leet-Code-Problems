c053d5a4bdb683661591bf6e05b0b33d255edf56
MdSomratAkbor <somrat876@gmail.com>
2023-04-09 15:13:51 +0600

    friend request ii  who has the friend problem solve

---
+++ friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,8 @@
+﻿select distinct  top 1 id, count(id) num
+from (
+ select requester_id as id, accepter_id as freind from RequestAccepted  
+    union
+select accepter_id as id, requester_id as freind from RequestAccepted 
+) x
+group by id
+order by num desc
