5e6ca0c58b51b9b4b15af965c2de48c93f8d1126
MdSomratAkbor <somrat876@gmail.com>
2023-04-10 13:53:31 +0600

    count-salary-categories problem solve

---
+++ count-salary-categories.sql
@@ -0,0 +1,14 @@
+﻿select 'Low Salary' category, count(account_id) accounts_count
+from Accounts 
+where income <20000
+
+union
+
+select 'Average Salary' category, count(account_id) accounts_count
+from Accounts 
+where income between 20000 and 50000
+
+union
+select 'High Salary' category, count(account_id) accounts_count
+from Accounts 
+where income > 50000
