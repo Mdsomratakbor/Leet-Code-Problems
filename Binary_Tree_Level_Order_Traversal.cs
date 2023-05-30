97c39e4531807aad9274df84e5226b02823cc6a2
MdSomratAkbor <somrat876@gmail.com>
2023-05-30 19:12:45 +0600

    binary tree order level traversal problem solve

---
+++ Binary_Tree_Level_Order_Traversal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Binary_Tree_Level_Order_Traversal
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+                List<int> currentLevel = new List<int>();
+
+                for (int i = 0; i < levelSize; i++)
+                {
+                    TreeNode currentNode = queue.Dequeue();
+                    currentLevel.Add(currentNode.val);
+
+                    if (currentNode.left != null)
+                        queue.Enqueue(currentNode.left);
+
+                    if (currentNode.right != null)
+                        queue.Enqueue(currentNode.right);
+                }
+
+                result.Add(currentLevel);
+            }
+            return result;
+
+        }
+    }
+}
