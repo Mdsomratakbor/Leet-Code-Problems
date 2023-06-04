92927f5c63d1078a35d4c929983a752bec662324
MdSomratAkbor <somrat876@gmail.com>
2023-06-04 16:20:14 +0600

    maximum binary tree path problem solve

---
+++ Maximum-Depth-of-Binary-Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    public class Maximum_Depth_of_Binary_Tree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            int maxPath = 0;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+                maxPath++;
+                for(int i=0; i<levelSize; i++)
+                {
+                    TreeNode currentNode = queue.Dequeue();
+                    if (currentNode.left != null)
+                        queue.Enqueue(currentNode.left);
+
+                    if (currentNode.right != null)
+                        queue.Enqueue(currentNode.right);
+                }
+            }
+            return maxPath;
+        }
+    }
+}
