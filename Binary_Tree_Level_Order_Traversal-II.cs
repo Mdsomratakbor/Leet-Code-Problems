0eb417a4595758e468b06d01be1efb705d821cb8
MdSomratAkbor <somrat876@gmail.com>
2023-06-04 17:57:19 +0600

    Binary_Tree_Level_Order_Traversal-II problem solve

---
+++ Binary_Tree_Level_Order_Traversal-II.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    internal class Binary_Tree_Level_Order_Traversal_II
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int levelSize = queue.Count;
+                List<int> currentLevel = new List<int>();
+
+                for (int i = 0; i < levelSize; i++)
+                {
+                    TreeNode currentNode = queue.Dequeue();
+                    currentLevel.Add(currentNode.val);
+
+                    if (currentNode.left != null)
+                        queue.Enqueue(currentNode.left);
+
+                    if (currentNode.right != null)
+                        queue.Enqueue(currentNode.right);
+                }
+
+                result.Add(currentLevel);
+            }
+            result = result.Reverse().ToList();
+            return result;
+
+        }
+    }
+}
