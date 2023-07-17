d0874d90231f4acf6a43b7b0b263b6f358e70904
Md Somrat Akbor <somrat876@gmail.com>
2023-07-18 01:25:54 +0600

    Reverse String problem solve

---
+++ Reverse_String.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Reverse_String
+    {
+        public void ReverseString(char[] s)
+        {
+            int left = 0;
+            int right = s.Length  -  1;
+            char temp = ' ';
+            while (left<right)
+            {
+                temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+            
+        }
+    }
+}
