7828ea38d0e8e172fac8b5cf8d2a4068a1878651
MdSomratAkbor <somrat876@gmail.com>
2023-07-18 19:32:57 +0600

    Climbing_Stairs problem solve

---
+++ Climbing_Stairs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Climbing_Stairs
+    {
+        public int ClimbStairs(int n)
+        {
+            int one = 1, two = 1;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int temp = one;
+                one += two;
+                two = temp;
+            }
+            return one;
+        }
+    }
+}
