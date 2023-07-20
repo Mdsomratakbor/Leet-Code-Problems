20c395e1b4914420e8e11d4ca3a647d4bec4d189
MdSomratAkbor <somrat876@gmail.com>
2023-07-20 17:51:47 +0600

    Remove_Trailing_Zeros_From_a_String problem solve

---
+++ Remove-Trailing-Zeros-From-a-String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Remove_Trailing_Zeros_From_a_String
+    {
+        public string RemoveTrailingZeros(string num)
+        {
+            for (int i = num.Length - 1; i>=0; i--)
+            {
+                if (num[i] != '0')
+                {
+                    num = num.Substring(0, i+=1);
+                    break;
+
+                }
+            }
+            return num;
+        }
+    }
+}
