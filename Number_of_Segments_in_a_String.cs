6ec35a75ed2d1894acf8ef00c2e01df543817176
Md Somrat Akbor <somrat876@gmail.com>
2023-07-24 00:30:05 +0600

    Number_of_Segments_in_a_String problem start

---
+++ Number_of_Segments_in_a_String.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Number_of_Segments_in_a_String
+    {
+        public int CountSegments(string s)
+        {
+            return s.Split(" ").Length;
+        }
+    }
+}
---
+++ RansomNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            return magazine.Contains(ransomNote);
+        }
+    }
+}
