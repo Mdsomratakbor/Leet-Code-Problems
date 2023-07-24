beb4fe656902da3cee79ddc71e55064fe3d08a94
MdSomratAkbor <somrat876@gmail.com>
2023-07-24 13:23:52 +0600

    TruncateSentence problem solve

---
+++ Truncate_Sentence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Truncate_Sentence
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            string[] sArray = s.Split(" ");
+            StringBuilder sb =new StringBuilder();
+            for(int i=0; i<k; i++)
+            {
+                if(sb.Length>0)
+                    sb.Append(" ");
+
+                sb.Append(sArray[i]);
+
+            }    
+            return sb.ToString();
+        }
+    }
+}
