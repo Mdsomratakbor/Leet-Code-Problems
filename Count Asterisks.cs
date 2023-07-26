90efb45d5008cd128fce7ef1b6cf26962a0a15bb
MdSomratAkbor <somrat876@gmail.com>
2023-07-26 14:23:44 +0600

    count asterisks problem solve

---
+++ Count Asterisks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Count_Asterisks
+    {
+        public int CountAsterisks(string s)
+        {
+            int totalAsterisks = 0;
+            int barCount = 0;
+
+            for(int i=0; i<s.Length; i++)
+            {
+                if (s[i] == '|') barCount++;
+                else 
+                    if(barCount%2==0 && s[i]=='*')totalAsterisks++;
+            }
+            return totalAsterisks;
+        }
+    }
+}
