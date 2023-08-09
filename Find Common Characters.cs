150257cb18f4f0afd5f626607845d32892049cc8
MdSomratAkbor <somrat876@gmail.com>
2023-08-09 19:52:48 +0600

    find the common character problme solving start

---
+++ Find Common Characters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Find_Common_Characters
+    {
+        public IList<string> CommonChars(string[] words)
+        {
+            IList<string> result = new List<string>();
+            foreach (char c in words[0])
+            {
+              
+                    bool isTrue = true;
+                    foreach (string word1 in words)
+                    {
+                        if (!word1.Contains(c))
+                        {
+                            isTrue = false;
+                            break;
+                        }
+                        
+                    }
+                    if (isTrue)
+                    {
+                        result.Add(c.ToString());
+                    }
+
+                    
+                
+            }
+            return result;
+
+        }
+    }
+}
