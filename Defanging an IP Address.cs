0e61e1eb2314058a616dff2a0b6fe40d5583cd37
MdSomratAkbor <somrat876@gmail.com>
2023-08-10 19:38:19 +0600

    Defanging_an_IP_Address problem solve

---
+++ Defanging an IP Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Defanging_an_IP_Address
+    {
+        public string DefangIPaddr(string address)
+        {
+        
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i=0; i<address.Length; i++)
+            {
+                
+                if (address[i] == '.')
+                {
+
+                    stringBuilder.Append("[.]");
+
+                } 
+                else
+                {
+                    stringBuilder.Append(address[i]);
+                }
+             
+            }
+
+
+            return stringBuilder.ToString() ;
+        }
+    }
+}
