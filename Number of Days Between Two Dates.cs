ac195cd791b5d7c17851199a9a270c2532797e2b
MdSomratAkbor <somrat876@gmail.com>
2023-08-10 19:44:08 +0600

    Number_of_Days_Between_Two_Dates problem solve

---
+++ Number of Days Between Two Dates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Number_of_Days_Between_Two_Dates
+    {
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            DateTime firstDate = Convert.ToDateTime(date1);
+            DateTime secondDate = Convert.ToDateTime(date2);
+            return Math.Abs((secondDate - firstDate).Days);
+        }
+    }
+}
