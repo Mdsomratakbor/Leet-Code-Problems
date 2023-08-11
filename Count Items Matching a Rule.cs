967625de6053e1ce785b6c2c3cbd4adfc299ce69
Md Somrat Akbor <somrat876@gmail.com>
2023-08-11 12:12:36 +0600

    Count_Items_Matching_a_Rule problem solve

---
+++ Count Items Matching a Rule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Count_Items_Matching_a_Rule
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            int count = 0;
+
+            if (ruleKey=="type")
+            {
+                foreach (var item in items)
+                {
+                    if (item[0]== ruleValue)
+                    {
+                        count++;
+                    }
+                }
+            }
+            else if (ruleKey == "color")
+            {
+                foreach (var item in items)
+                {
+                    if (item[1]== ruleValue)
+                    {
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    if (item[2]== ruleValue)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+
+        }
+    }
+}
