3862e00d2e1d527eacce8a13866c9fa4dfc117b0
MdSomratAkbor <somrat876@gmail.com>
2023-08-13 16:12:23 +0600

    Student_Attendance_Record_I problem solve

---
+++ Reformat Date.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Reformat_Date
+    {
+        public string ReformatDate(string date)
+        {
+
+            return "";
+        }
+    }
+}
---
+++ Student Attendance Record I.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Student_Attendance_Record_I
+    {
+        public bool CheckRecord(string s)
+        {
+            int absenseCount = 0;
+            for (int i=0; i<s.Length; i++)
+            {
+
+                if ( s[i] == 'A')
+                {
+                    absenseCount++;
+                    if (absenseCount >= 2)
+                        return false;
+                }
+                else if (s[i] == 'L')
+                {
+                    int lateCount = 0;
+                    for (int j = i; j < s.Length; j++)
+                    {
+
+                        if (s[i] != s[j])
+                        {
+                            break;
+                        }
+                       else if (s[i] == s[j] && s[i] == 'L')
+                        {
+                            lateCount++;
+                            if (lateCount >= 3)
+                                return false;
+                        }
+                    }
+                }
+             
+              
+
+
+
+            }
+            return true;
+        }
+    }
+}
