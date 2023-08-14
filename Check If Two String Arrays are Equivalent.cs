c448e936b0ac1b78b3046c4a15e617ee653034cf
MdSomratAkbor <somrat876@gmail.com>
2023-08-14 16:14:24 +0600

    Check_If_Two_String_Arrays_are_Equivalent problem solve

---
+++ Check If Two String Arrays are Equivalent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Check_If_Two_String_Arrays_are_Equivalent
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            foreach (string word in word1)
+            {
+                sb.Append(word);
+            }
+            foreach (string wor2 in word2)
+            {
+                sb2.Append(wor2);
+            }
+            return sb.ToString().Equals(sb2.ToString());
+        }
+    }
+}
