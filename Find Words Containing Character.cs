29646ba6baa410a8d7b0119c0bbd14bfa92da475
MdSomratAkbor <somrat876@gmail.com>
2024-11-10 18:11:08 +0600

    Find_Words_Containing_Character problem solve

---
+++ Find Words Containing Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Find_Words_Containing_Character
+    {
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            List<int> result = new List<int>();
+
+            for(int i=0; i<words.Length; i++)
+            {
+                if (words[i].Contains(x))
+                {
+                    result.Add(i); 
+                }
+            }
+            return result;
+        }
+    }
+}
