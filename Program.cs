b4b505bd442bf2c4140bd4713fb6db9a3760cc7a
Md Somrat Akbor <somrat876@gmail.com>
2024-11-12 15:32:15 +0600

    implement new problem

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace LeetCodeProblems
         static void Main(string[] args)
         {
 
-            Rotate_String nameOfClass = new();
+            Counting_Words_With_a_Given_Prefix nameOfClass = new();
 
             //            TreeNode root = new TreeNode(2);
             //            root.right = new TreeNode(3);
@@ -45,7 +45,7 @@ namespace LeetCodeProblems
             // 
             //   var data = new int[] {  1, 1, 0, 1, 99,99,99 };
             //var data2 = nameOfClass.SingleNumber(data);
-            var data2 = nameOfClass.RotateString("abcde", "cdeab");
+            var data2 = nameOfClass.PrefixCount( ["pay", "attention", "practice", "attend"],  "at");
 
             Console.WriteLine(data2);
 
