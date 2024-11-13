8f9ae5b4ad06530d04bff096b4c754a01c92d5d7
MdSomratAkbor <somrat876@gmail.com>
2024-11-13 13:56:29 +0600

    sorting sentence problem solving

---
+++ Sorting the Sentence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Sorting_the_Sentence
+    {
+        public string SortSentence(string s)
+        {
+            string[] orginalSentence = s.Split(' ');
+            List<Tuple<string, int , bool>> keyValuePairs = new List<Tuple<string, int , bool>>();
+            foreach (var word in orginalSentence)
+            {
+
+            }
+            string temp = "";
+            int left = 0, right = orginalSentence.Length;
+            while (left<right)
+            {
+                int index = Int32.Parse(orginalSentence[left][orginalSentence[left].Length-1].ToString());
+                temp = orginalSentence[index-1];
+                orginalSentence[index-1] = orginalSentence[left];
+                orginalSentence[left] = temp;
+                left++;
+            }
+
+            string result = "";
+            foreach (string word in  orginalSentence)
+            {
+                result += word.Substring(0, word.Length-1) + " ";
+            }
+
+            return result.TrimEnd();
+
+        }
+    }
+}
