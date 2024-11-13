53d9d6fd101b22a1ef55897c81c70e74aac07b2b
MdSomratAkbor <somrat876@gmail.com>
2024-11-13 18:30:35 +0600

    Long Pressed name problem solve

---
+++ Long Pressed Name.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Long_Pressed_Name
+    {
+        public bool IsLongPressedName(string name, string typed)
+        {
+            int left =0, right = 0;
+            while (right<typed.Length)
+            {
+                if(left<name.Length && name[left]== typed[right])
+                {
+                    right++;
+                    left++;
+                }
+                else if(right>0 && typed[right] == typed[right - 1])
+                {
+                    right++;
+                }
+                else
+                {
+                    // If no match and no repeat, it's not a valid long press
+                    return false;
+                }
+
+            }
+            return left == name.Length;
+        }
+            public bool IsLongPressedNameOld(string name, string typed)
+        {
+            Dictionary<char, int> mapNameCharacters= new Dictionary<char, int>();
+            Dictionary<char, int> mapTypedCharacters = new Dictionary<char, int>();
+
+            foreach (char c in typed)
+            {
+                if (mapTypedCharacters.ContainsKey(c))
+                {
+                    mapTypedCharacters[c] +=1;
+                }
+                else
+                {
+                    mapTypedCharacters.Add(c, 1);
+                }
+            }
+            
+            foreach (char c in name)
+            {
+                if (mapNameCharacters.ContainsKey(c))
+                {
+                    mapNameCharacters[c] += 1;
+                }
+                else
+                {
+                    mapNameCharacters.Add(c, 1);
+                }
+            }
+
+
+
+            foreach (var keyValuePair in mapNameCharacters)
+            {
+                int index = GetIndexByKey(mapTypedCharacters, keyValuePair.Key);
+                int index2 = GetIndexByKey(mapNameCharacters, keyValuePair.Key);
+                if (index == -1 || index2 == -1)
+                    return false;
+
+                if (index != index2)
+                    return false;
+                if (!mapTypedCharacters.ContainsKey(keyValuePair.Key) || mapTypedCharacters[keyValuePair.Key] < keyValuePair.Value)
+                    return false;
+            }
+
+
+
+            return true;
+
+        }
+
+        int GetIndexByKey(Dictionary<char, int> dictionary, char key)
+        {
+            int index = 0;
+            foreach (var kvp in dictionary)
+            {
+                if (kvp.Key == key)
+                    return index;
+                index++;
+            }
+            return -1; // Return -1 if the key is not found
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace LeetCodeProblems
         static void Main(string[] args)
         {
 
-            Delete_Characters_to_Make_Fancy_String nameOfClass = new();
+            Long_Pressed_Name nameOfClass = new();
 
             //            TreeNode root = new TreeNode(2);
             //            root.right = new TreeNode(3);
@@ -45,7 +45,7 @@ namespace LeetCodeProblems
             // 
             //   var data = new int[] {  1, 1, 0, 1, 99,99,99 };
             //var data2 = nameOfClass.SingleNumber(data);
-            var data2 = nameOfClass.MakeFancyString("aaabaaaa");
+            var data2 = nameOfClass.IsLongPressedName("rick", "kric");
 
             Console.WriteLine(data2);
 
