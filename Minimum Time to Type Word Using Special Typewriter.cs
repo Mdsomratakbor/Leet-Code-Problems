f96a34425d132518117fa554b6e42a8b95ae25c9
Md Somrat Akbor <somrat876@gmail.com>
2024-11-22 23:32:28 +0600

    new problem solve

---
+++ Minimum Time to Type Word Using Special Typewriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Minimum_Time_to_Type_Word_Using_Special_Typewriter
+    {
+        public int MinTimeToType(string word)
+        {
+            int time = 0;
+            char current = 'a';  
+
+            foreach (char c in word)
+            {
+                int distance = Math.Abs(c - current);
+                int minValue = Math.Min(distance, 26 - distance);
+                time += minValue + 1;
+                current = c;
+            }
+            return time;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace LeetCodeProblems
         static void Main(string[] args)
         {
 
-            Keyboard_Row nameOfClass = new();
+            Minimum_Time_to_Type_Word_Using_Special_Typewriter nameOfClass = new();
 
             //            TreeNode root = new TreeNode(2);
             //            root.right = new TreeNode(3);
@@ -45,7 +45,7 @@ namespace LeetCodeProblems
             // 
             //   var data = new int[] {  1, 1, 0, 1, 99,99,99 };
             //var data2 = nameOfClass.SingleNumber(data);
-            var data2 = nameOfClass.FindWords(["Hello", "Alaska", "Dad", "Peace"]);
+            var data2 = nameOfClass.MinTimeToType("abc");
 
             Console.WriteLine(data2);
 
