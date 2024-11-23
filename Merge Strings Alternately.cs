ee455947c024b04014767c499c61158e07d90f1b
Md Somrat Akbor <somrat876@gmail.com>
2024-11-23 15:20:37 +0600

    some new problem solve

---
+++ Merge Strings Alternately.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Merge_Strings_Alternately
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+
+            int left = 0, rightWord1 = word1.Length, rightWord2 = word2.Length;
+
+            int right = Math.Max(rightWord1, rightWord2);
+
+            StringBuilder result = new StringBuilder();
+            while (left < right)
+            {
+                if(left<rightWord1)
+                {
+                    result.Append(word1[left]);
+                }
+                if (left < rightWord2)
+                {
+                    result.Append(word2[left]);
+                }
+                left++;
+            }
+            return result.ToString();   
+        }
+
+        public string MergeAlternately_2(string word1, string word2)
+        {
+
+            int left = 0, rightWord1 = word1.Length, rightWord2 = word2.Length;
+
+            StringBuilder result = new StringBuilder();
+            while (left < rightWord1 || left < rightWord2)
+            {
+                if (left < rightWord1)
+                {
+                    result.Append(word1[left]);
+                }
+                if (left < rightWord2)
+                {
+                    result.Append(word2[left]);
+                }
+                left++;
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Reformat Phone Number.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Reformat_Phone_Number
+    {
+        public string ReformatNumber(string number)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int length = 0;
+            foreach (char c in number)
+            {
+                if (char.IsDigit(c))
+                {
+                    stringBuilder.Append(c);
+                    length++;
+                }
+             
+
+            }
+            int index = 3;
+            while (length>4)
+            {
+                if (length > 4)
+                {
+                    stringBuilder.Insert(index, '-');
+                    index += 4;
+                    length -= 3;
+                }
+          
+            }
+            if (length == 4)
+            {
+                stringBuilder.Insert(index - 1, '-');
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
