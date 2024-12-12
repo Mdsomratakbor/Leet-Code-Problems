364955937bd7577eb39fe6a215e9a34f30524e51
MdSomratAkbor <somrat876@gmail.com>
2024-12-12 23:56:06 +0600

    two new problme solve

---
+++ Check If a Word Occurs As a Prefix of Any Word in a Sentence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            string[] arrayOfString = sentence.Split(' ');
+            int count = 1;
+            foreach (string word in arrayOfString)
+            {
+                if (word.StartsWith(searchWord))
+                {
+                    return count;
+                }
+                count++;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Determine if String Halves Are Alike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    internal class Determine_if_String_Halves_Are_Alike
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            s = s.ToLower();
+            int firstHalfVowel = 0, secondHalfVowel = 0, left = 0, half = 0, right = 0, length = 0;
+
+            length = s.Length;
+            right = length - 1;
+            half = length / 2;
+
+            while (left<half &&  right>=half)
+            {
+                if (s[left] == 'a' || s[left] == 'e' || s[left] == 'i' || s[left] == 'o' || s[left] == 'u')
+                    firstHalfVowel++;
+                if (s[right] == 'a' || s[right] == 'e' || s[right] == 'i' || s[right] == 'o' || s[right] == 'u')
+                    secondHalfVowel++;
+                left++;
+                right--;
+            }
+
+
+            return firstHalfVowel == secondHalfVowel;
+        }
+    }
+}
